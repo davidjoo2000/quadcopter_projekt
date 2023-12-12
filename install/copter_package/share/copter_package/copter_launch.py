91843707709ba416624c12bd979cb31014859daa
davidjoo2000 <joo.david53@gmail.com>
2023-12-12 10:59:04 +0100

    nem sikerult :(

--- install/copter_package/share/copter_package/copter_launch.py
+++ install/copter_package/share/copter_package/copter_launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     return LaunchDescription([
@@ -10,12 +11,16 @@ def generate_launch_description():
             cmd=['ign', 'gazebo', '-v', '4', '-r', 'kopter.sdf', '--render-engine', 'ogre'],
             output='screen'
         ),
-
+        ExecuteProcess(
+        cmd=['ros2','run ros_gz_bridge parameter_bridge /world/quadcopter/pose/info@geometry_msgs/msg/Pose@ignition.msgs.Pose'],
+        output='screen',
+        shell=True
+),
         Node(
             package='copter_package',
             executable='copter_node',
             name='copter',
             output='screen',
-            parameters=[{'move_up_height': '2.0'}]
+            parameters=[{'move_up_height': LaunchConfiguration('move_up_height')}]
         )
     ])
--- src/copter_package/copter_package/copter_node.py
+++ src/copter_package/copter_package/copter_node.py
@@ -1,33 +1,33 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Pose
-from std_msgs.msg import Float64
 
-class MyNode(Node):
+
+class Copter(Node):
     def __init__(self):
         super().__init__('copter_node')
-        self.move_up_pub = self.create_publisher(Float64, '/world/quadcopter/pose', 10)
-        self.pose_sub = self.create_subscription(Pose, '/world/quadcopter/pose', self.pose_callback, 10)
+        self.move_up_pub = self.create_publisher(Pose, '/world/quadcopter/pose', 10)
+        self.pose_sub = self.create_subscription(Pose, '/world/quadcopter/pose/info', self.pose_callback, 10)
 
         # Read the launch argument
+        self.declare_parameter('move_up_height', 2.0)
         self.move_up_height = self.get_parameter('move_up_height').value
 
         self.move_up()
 
-
     def move_up(self):
-        msg = Float64()
-        msg.data = self.move_up_height
+        msg = Pose()
+        msg.position.z = float(self.move_up_height)
         self.move_up_pub.publish(msg)
 
     def pose_callback(self, msg):
         self.get_logger().info(f"Current Drone Position: x={msg.position.x}, y={msg.position.y}, z={msg.position.z}")
+        self.get_logger().info(f"Received Pose Message: {msg}")
 
 
 def main(args=None):
-    
     rclpy.init(args=args)
-    node = MyNode()
+    node = Copter()
     rclpy.spin(node)
     rclpy.shutdown()
 
